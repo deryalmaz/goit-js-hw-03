290f6ffecc19f8a03e0db9179889d3e7639b6d6f
Derya Almaz <deryaalmazz@icloud.com>
2024-11-21 11:46:32 +0300

    task-2 is completed

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,6 @@
-function slugify (title) {
-    return title.toLowerCase().split(" ").join("-");
-  }
-  
-
+function slugify(title) {
+  return title.toLowerCase().split(" ").join("-");
+}
 
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
